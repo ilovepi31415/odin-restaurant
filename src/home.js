1c26e99b6235e2a02a5044b8ce62a30948103ab5
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-05-14 15:48:07 -0400

    Add homepage to js file

---
+++ src/home.js
@@ -0,0 +1,30 @@
+import spaghetti from "./images/spaghetti.jpg";
+
+function createPage() {
+	const container = document.querySelector("#content");
+
+	const header = document.createElement("h1");
+	header.innerText = "Welcome to my restaurant!";
+	const subtitle = document.createElement("h3");
+	subtitle.innerText = "it's the best ever";
+	const hr = document.createElement("hr");
+	const quoteContainer = document.createElement("div");
+	quoteContainer.setAttribute("id", "quote-container");
+	const img = document.createElement("img");
+	img.src = spaghetti;
+	img.setAttribute("alt", "spaghetti");
+	const quote = document.createElement("p");
+	quote.setAttribute("id", "quote");
+	quote.innerText =
+		"This restaurant is the best one I've ever seen! I would reccommend this to everyone";
+
+	quoteContainer.appendChild(img);
+	quoteContainer.appendChild(quote);
+
+	container.appendChild(header);
+	container.appendChild(subtitle);
+	container.appendChild(hr);
+	container.appendChild(quoteContainer);
+}
+
+export default createPage;
