d0e67018f1ffe62438954c9378274c931fdf345a
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-05-18 17:36:56 -0400

    Update buttons based on active page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./style.css";
 import createHomePage from "./home.js";
 import createMenu from "./menu.js";
 
+const activeColor = "#fefae0";
+
 const container = document.querySelector("#content");
 const home = document.querySelector("#btn-home");
 const menu = document.querySelector("#btn-menu");
@@ -9,11 +11,16 @@ const menu = document.querySelector("#btn-menu");
 home.addEventListener("click", () => {
 	container.replaceChildren();
 	createHomePage();
+	home.style.backgroundColor = activeColor;
+	menu.style.backgroundColor = "";
 });
 
 menu.addEventListener("click", () => {
 	container.replaceChildren();
 	createMenu();
+	menu.style.backgroundColor = activeColor;
+	home.style.backgroundColor = "";
 });
 
 createHomePage();
+home.style.backgroundColor = activeColor;
